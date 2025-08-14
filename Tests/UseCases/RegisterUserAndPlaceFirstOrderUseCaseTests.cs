996ee9de9bc26fc34bb800946592a3692c146a55
Omid-Sargazi <om.sargazi@gmail.com>
2025-08-14 09:57:34 +0330

    Unit Test in Should_Fail_When_NoItemsForOrder

--- Tests/UseCases/RegisterUserAndPlaceFirstOrderUseCaseTests.cs
+++ Tests/UseCases/RegisterUserAndPlaceFirstOrderUseCaseTests.cs
@@ -29,5 +29,53 @@ namespace Tests.UseCases
             var result = useCase.Execute(userRequest, orderRequest);
             result.Should().Be("User registered and first order placed");
         }
+
+        [Fact]
+        public void Should_Fail_When_UserAlreadyExists()
+        {
+            var userRepo = new InMemoryUserRepository();
+            var orderRepo = new InMemoryOrderRepository();
+            var useCase = new RegisterUserAndPlaceFirstOrderUseCase(userRepo, orderRepo);
+
+            var userRequest = new RegisterUserRequest
+            {
+                Email = "duplicate@example.com",
+                Password = "pass1234"
+            };
+
+            var orderRequest = new PlaceOrderRequest
+            {
+                Items = new List<string> { "item" },
+                TotalAmount = 50
+            };
+
+            useCase.Execute(userRequest, orderRequest);
+
+            var result = useCase.Execute(userRequest, orderRequest);
+            result.Should().Be("User already exists");
+        }
+        [Fact]
+        public void Should_Fail_When_NoItemsForOrder()
+        {
+            var useRepo = new InMemoryUserRepository();
+            var orderRepo = new InMemoryOrderRepository();
+            var useCase = new RegisterUserAndPlaceFirstOrderUseCase(useRepo, orderRepo);
+
+            var userRequest = new RegisterUserRequest
+            {
+                Email = "o@o.com",
+                Password = "pass1234"
+            };
+
+            var orderRequest = new PlaceOrderRequest
+            {
+                Items = new List<string>(),
+                TotalAmount = 0
+            };
+
+            var result = useCase.Execute(userRequest, orderRequest);
+
+            result.Should().Be("No items for order");
+        }
     }
 }
